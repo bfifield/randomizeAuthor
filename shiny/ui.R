1a7c49096d2263789b97daf39de13cecce2d9b60
Ben Fifield <benfifield@gmail.com>
2017-08-19 21:16:30 -0700

    new deployment with more detail in instructions

--- shiny/ui.R
+++ shiny/ui.R
@@ -19,7 +19,7 @@ shinyUI(fluidPage(
             textInput("name8", label = "", value = ""),
 
             ## Input seed
-            textInput("seed", label = "Random Seed (Optional)", value = "")## ,
+            textInput("seed", label = "Random Seed (Only Provide when Replicating Randomization)", value = "")## ,
             
         ),
 
@@ -28,7 +28,7 @@ shinyUI(fluidPage(
             h2("Welcome to randomizeAuthor!", align = "center"),
             
             h3("Instructions:"),
-            HTML("<ul><li>Input email addresses of at least one recipient under <strong>Recipient Emails</strong>. Separate each email with a semicolon or a comma. For example, <em>authorA@univ.edu; authorB@coll.edu</em>. A copy of the randomization results will also be sent to <em>randomizeauthor@gmail.com</em>.</li><li>Input author names in blank fields under <strong>Author Names</strong>.</li><li>Hit the <strong>Randomize</strong> button below, which will return the randomized author ordering and the random seed to replicate the results. Results will be emailed to the provided email addresses from <em>randomizeauthor@gmail.com</em>.</li><li>To replicate results, input author names in the same order, input the random seed in <strong>Random Seed</strong>, and hit the <strong>Randomize</strong> button below.</li></ul>"),
+            HTML("<ul><li>Input email addresses of at least one recipient under <strong>Recipient Emails</strong>. Separate each email with a semicolon or a comma. For example, <em>authorA@univ.edu; authorB@coll.edu</em>. A copy of the randomization results will also be sent to <em>randomizeauthor@gmail.com</em>.</li><li>Input author names in blank fields under <strong>Author Names</strong>. Only put one name in each field.</li><li>Hit the <strong>Randomize</strong> button below, which will return the randomized author ordering and the random seed drawn from <em>random.org</em> that can be used to replicate the results. Results will be emailed to the provided email addresses from <em>randomizeauthor@gmail.com</em>.</li><li>To replicate results, input author names in the same order, input the random seed in the <strong>Random Seed</strong> field, and hit the <strong>Randomize</strong> button below.</li></ul>"),
 
             p(),
 
